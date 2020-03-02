17905f5d6fca6f7c85bd5a639a6e4d1b92089ca7
Thiago Lopes da Silva <t.a.lopes.da.silva@avanade.com>
2020-03-02 18:29:51 -0300

    first commit my study vuetify

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>Dashboard</h1>
+        <v-data-table
+        :headers="headers"
+        :items="desserts"
+        :items-per-page="5"
+        class="elevation-1"
+        ></v-data-table>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
